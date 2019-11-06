90a3c8d88ad83401aeeb62eb4205f35fe48863e6
nnzhaocs <znannan1@vt.edu>
2019-11-06 14:34:53 -0500

    update

--- loadtest/load-test-0.1.1/locustfile.py
+++ loadtest/load-test-0.1.1/locustfile.py
@@ -10,18 +10,18 @@ class WebTasks(TaskSet):
     def load(self):
         base64string = base64.encodestring('%s:%s' % ('user', 'password')).replace('\n', '')
 
-        #catalogue = self.client.get("/catalogue").json()
-        #category_item = choice(catalogue)
-        #item_id = category_item["id"]
+        catalogue = self.client.get("/catalogue").json()
+        category_item = choice(catalogue)
+        item_id = category_item["id"]
 
-        #self.client.get("/")
+        self.client.get("/")
         self.client.get("/login", headers={"Authorization":"Basic %s" % base64string})
-        #self.client.get("/category.html")
-        #self.client.get("/detail.html?id={}".format(item_id))
-        #self.client.delete("/cart")
-        #self.client.post("/cart", json={"id": item_id, "quantity": 1})
-        #self.client.get("/basket.html")
-        #self.client.post("/orders")
+        self.client.get("/category.html")
+        self.client.get("/detail.html?id={}".format(item_id))
+        self.client.delete("/cart")
+        self.client.post("/cart", json={"id": item_id, "quantity": 1})
+        self.client.get("/basket.html")
+        self.client.post("/orders")
 
 
 class Web(HttpLocust):
